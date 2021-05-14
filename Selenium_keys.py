436255898af64aa7d1f959bdc38687e601730703
gustavo.cn121 <gustavo.cn121@gmail.com>
2021-05-14 17:56:12 -0300

    a

--- Selenium_keys.py
+++ Selenium_keys.py
@@ -1,3 +1,3 @@
-PATH = "C:/Users/gusta/Desktop/Crwler2.0/selenium_driver/chromedriver.exe"
-tt_username = 'crwler2'
-tt_password = 'Crawler123456'
+PATH = "./selenium_driver/chromedriver.exe"
+tt_username = ''
+tt_password = ''
